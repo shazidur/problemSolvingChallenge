44bbd9b3e5c3001661bb7101ce4bfd96dee44664
Md.Shazidur Rahman <jadushazid007@gmail.com>
2020-05-09 19:34:42 +0900

    add new array related problem.

---
+++ hakerRank/Arre/array.js
@@ -0,0 +1,29 @@
+// problem type : easy
+// problem link https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+const a = [5, 9, 10, 33];
+const b = [3, 6, 11, 44];
+
+let Alice = [];
+let Bob = [];
+
+let index = 0;
+for (let aValue of a) {
+  if (aValue > b[index]) {
+    console.log(" Alise got a point 1 for the value of : ", aValue);
+    Alice.push(1);
+  }
+
+  if (aValue < b[index]) {
+    console.log(" Bob got a point 1 for the value of ", aValue);
+    Bob.push(1);
+  }
+
+  index += 1;
+}
+
+const winAlice = Alice.length;
+const winBob = Bob.length;
+
+const finalOutput = [winAlice, winBob];
+console.log("Final outout is :", finalOutput);
